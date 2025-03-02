8135f3d66795e93ed5f356c66168b1a1044a79c2
Shristi-Bajra <shreestybajracharya@gmail.com>
2025-03-02 11:17:07 +0545

    Updated websocket

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const { Server } = require("socket.io");
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
-const io = new Server(server);
+// const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Change this to your frontend URL in production
+    methods: ["GET", "POST"],
+  },
+});
 
 const userSocketMap = {};  // Maps socket IDs to usernames
 
